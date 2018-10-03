fff5dbcf821044d56c850ce04c8e2225ea46178e
hari <hkrishna@taxbackinternational.com>
2018-10-03 23:17:48 +0530

    animation added

--- moveemback/src/app/components/tab-holder/communications/communications.component.ts
+++ moveemback/src/app/components/tab-holder/communications/communications.component.ts
@@ -1,10 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { communicationSettings } from './communication.setting';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-communications',
   templateUrl: './communications.component.html',
-  styleUrls: ['./communications.component.scss']
+  styleUrls: ['./communications.component.scss'],
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateX(100%)', opacity: 0}),
+          animate('200ms', style({transform: 'translateY(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)', opacity: 1}),
+          animate('100ms', style({transform: 'translateY(100%)', opacity: 0}))
+        ])
+      ]
+    )
+  ],
 })
 export class CommunicationsComponent implements OnInit {
 
